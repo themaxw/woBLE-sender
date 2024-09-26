08d9a5d29b6e7e8d0bd76c963f2d3c77cbab0fe4
Max Weidauer <max+git@weidauer.art>
2024-09-26 02:55:26 +0200

    added esp now

---
+++ include/Message.h
@@ -0,0 +1,10 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+struct Message{
+    float rot_x;
+    float rot_y;
+    float rot_z;
+};
+
+#endif //MESSAGE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,71 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_MPU6050.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Message.h>
 
 Adafruit_NeoPixel rgb_led(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 Adafruit_MPU6050 mpu;
 
-unsigned long t0 = millis();
-bool isConnected = false;
-
-
-// put function declarations here:
-int myFunction(int, int);
+unsigned long timer = 0;
+float timestep = 0.01;
 
 float rotX, rotY, rotZ;
+Message message;
+
+uint8_t baseStationAddress[] = {0x4C, 0x75, 0x25, 0x37, 0x2D, 0xB1};
+esp_now_peer_info_t baseStationPeerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  // Serial.print("\r\nLast Packet Send Status:\t");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
+
 
   rgb_led.begin();
   rgb_led.setBrightness(70);
   rgb_led.show();
 
   Serial.begin(115200);
-  while (!Serial)
-    delay(10);
+  
+
+  WiFi.mode(WIFI_STA);
+  
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(baseStationPeerInfo.peer_addr, baseStationAddress, 6);
+  baseStationPeerInfo.channel = 0;  
+  baseStationPeerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&baseStationPeerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
   if (!mpu.begin()) {
-      Serial.println("Failed to find MPU6050 chip");
-      while (1) {
-        delay(10);
-      }
-    }
-    Serial.println("MPU6050 Found!");
-
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
+    //TODO error state irgendwie
+    Serial.println("Failed to find MPU6050 chip");
   }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  Serial.println("");
-
+  
   rotX = 0;
   rotY = 0;
   rotZ = 0;
@@ -104,18 +77,23 @@ void setup() {
 
 int rotation_to_color(float rotation){
   float ratio = rotation / (2*PI);
-  Serial.println(ratio);
   if (ratio < 0) ratio = 0;
   else if (ratio > 1) ratio = 1;
   return 255*ratio;
 }
 
 
-unsigned long timer = 0;
-float timestep = 0.01;
-
 void loop() {
   timer = millis();
+
+  float measuredvbat = analogReadMilliVolts(BATT_MONITOR);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat /= 1000; // convert to volts!
+  //TODO calculate battery charge from Vbat
+  // Serial.print("Battery Voltage: ");
+  // Serial.println(measuredvbat);
+
+
 
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
@@ -126,12 +104,20 @@ void loop() {
   rgb_led.setPixelColor(0, rgb_led.Color(rotation_to_color(rotX), rotation_to_color(rotY), rotation_to_color(rotZ)));
   rgb_led.show();
   // Serial.println(rotX);
+  message.rot_x = rotX;
+  message.rot_y = rotY;
+  message.rot_z = rotZ;
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(baseStationAddress, (uint8_t *) &message, sizeof(message));
+   
+  // if (result == ESP_OK) {
+  //   Serial.println("Sent with success");
+  // }
+  // else {
+  //   Serial.println("Error sending the data");
+  // }
+
   delay((timestep*1000) - (millis() - timer));
   
  
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
